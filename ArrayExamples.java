8e134ca628abc6a73bcd7d014a58362af2c0f0ef
yuribling123 <122572429+yuribling123@users.noreply.github.com>
2023-01-26 12:58:53 -0500

    fix method

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,9 +4,16 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+    //deep copy of arr
+    int[] copy = new int[arr.length];
+    for(int i=0;i<copy.length;i++){
+      copy[i]=arr[i];
     }
+  // change the index of arr
+    for(int i = 0; i < arr.length; i += 1) {
+      arr[i]=copy[copy.length-1-i];
+    }
+    return;
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
@@ -14,9 +21,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return  newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,21 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  
+  @Test
+  public void testReversedMine(){
+    int [] input1={1,2,3,4};
+    assertArrayEquals(new int[]{4,3,2,1}, ArrayExamples.reversed(input1));
+
+  }
+
+  @Test
+  public void testReversedInPlace(){
+    //Changes the input array to be in reversed order
+    int [] input1={1,2,3,4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,3,2,1}, input1);
+
+  }
+  
 }
